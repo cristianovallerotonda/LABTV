16377bb50bfbc7b2ac182aae57f0250c27980f3e
Cristiano <cristiano_4@libero.it>
2022-06-17 17:13:00 +0200

    feat: add page layout and content

--- src/app/services/apifilm.service.ts
+++ src/app/services/apifilm.service.ts
@@ -8,8 +8,8 @@ import { Observable } from 'rxjs';
 export class ApifilmService {
   constructor(private http: HttpClient) {}
 
-  wsApiFilm = 'https://imdb-api.com/en/API/MostPopularMovies/k_mtl1ukx1';
-  wsFilmDetail = 'https://imdb-api.com/it/API/Title/k_mtl1ukx1/';
+  wsApiFilm = 'https://imdb-api.com/en/API/MostPopularMovies/k_pfx1xif6';
+  wsFilmDetail = 'https://imdb-api.com/it/API/Title/k_pfx1xif6/';
 
   getApiFilm(): Observable<any> {
     return this.http.get(this.wsApiFilm);
@@ -21,3 +21,4 @@ export class ApifilmService {
 }
 
 //https://imdb-api.com/en/API/MostPopularMovies/k_mtl1ukx1
+// k_pfx1xif6
